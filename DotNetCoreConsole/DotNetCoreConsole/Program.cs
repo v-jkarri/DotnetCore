55b5a958a28c336c1d38f12572965819c60ad6ee
Janaki Priya Karri <v-jkarri@microsoft.com>
2021-09-02 13:52:47 +0530

    push

---
+++ DotNetCoreConsole/DotNetCoreConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
